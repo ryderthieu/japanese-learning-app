a3cbf52d2b5de07d9a2ad45b768aad2bf6c5c505
Tran Nhat Truong <22521584@gm.uit.edu.vn>
2024-12-12 19:39:13 +0700

    update login screen

--- frontend/src/navigations/AuthStack.jsx
+++ frontend/src/navigations/AuthStack.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Login from '../screens/Login'
-import Register from '../screens/Register'
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
 
 const AuthStack = () => {
-    const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator screenOptions={{
-        headerShown: false
-    }}>
-        <Stack.Screen name='Register' component={Register} />
-        <Stack.Screen name='Login' component={Login} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default AuthStack
-
+export default AuthStack;
--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,15 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Input, Icon, Button, SocialIcon } from '@rneui/themed';
 
 const Login = () => {
   return (
     <SafeAreaView>
-      <Text>Login</Text>
+      <View className='flex flex-col items-center w-screen h-screen px-10 py-16 bg-[#F3F6F8]'>
+        <Image source={require('../assets/logo/logo-app.png')}></Image>
+        <Text className='mt-10 mb-10 text-4xl font-bold text-[#2B308B]'>Đăng nhập</Text>
+        <View className='flex flex-col gap-5'>
+          <Input
+            containerStyle={{width: 'full',height: 60,backgroundColor: 'white', borderRadius: 20}}
+            placeholder='Email'
+            leftIcon={{ type: 'ant-design', name: 'user'}}
+          />
+          <Input
+            containerStyle={{width: 'full',height: 60,backgroundColor: 'white', borderRadius: 20}}
+            placeholder='Mật khẩu'
+            leftIcon={{ type: 'feather', name: 'key'}}
+          />
+          <Text className='text-right text-base text-[#2B308B]'>Quên mật khẩu?</Text>
+          <Button 
+            title={'Đăng nhập'}
+            buttonStyle={{width: 350, height: 50, backgroundColor: '#F490AF', borderRadius: 20}}
+          />
+        </View>
+        <Text className='mt-10 mb-5 text-base'>Hoặc đăng nhập bằng</Text>
+        <View className='flex flex-row mb-5 '>
+          <SocialIcon type='facebook' raise={false}/>
+          <SocialIcon type='google' raise={false}/>
+          <SocialIcon type='apple' light={true} raise={false}/>
+        </View>
+        <View className='flex flex-row gap-2 text-base'>
+          <Text>Chưa có tài khoản?</Text>
+          <Text className='text-[#2B308B] font-bold'>Đăng ký</Text>
+        </View>
+
+      </View>
+
+
+
+
+
+
+
+
     </SafeAreaView>
   )
 }
 
 export default Login
-
-const styles = StyleSheet.create({})
