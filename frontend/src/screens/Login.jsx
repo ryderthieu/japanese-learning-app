5bec7ec0a3ae628d082ea249b567ba86ddd626ac
Tran Nhat Truong <22521584@gm.uit.edu.vn>
2024-12-12 22:52:53 +0700

    update register screen

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -4,41 +4,53 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Input, Icon, Button, SocialIcon } from "@rneui/themed";
 import { useState } from "react";
 
-const Login = () => {
+const Login = ({navigation}) => {
   const [isPasswordVisible, setPasswordVisible] = useState(false);
 
   return (
 
     <SafeAreaView>
-      <View className="flex flex-col items-center w-screen h-screen px-10 py-16 bg-[#F3F6F8]">
+      <View className="flex flex-col items-center w-screen h-screen px-10 py-16">
         <Image source={require("../assets/logo/logo-app.png")}></Image>
         <Text className="mt-10 mb-10 text-4xl font-bold text-[#2B308B]">
           Đăng nhập
         </Text>
-        <View className="flex flex-col gap-5">
+        <View className="flex flex-col">
           <Input
             containerStyle={{
-              width: "full",
+              width: 'full',
+              paddingHorizontal: 0,
+            }}
+            inputContainerStyle={{
+              width: '100%',
               height: 60,
               backgroundColor: "white",
+              borderBottomColor: 'white',
               borderRadius: 20,
+              paddingHorizontal: 10
             }}
-            placeholder="Email"
+            placeholder="Địa chỉ email"
             leftIcon={{ type: "ant-design", name: "user" }}
           />
           <Input
             containerStyle={{
-              width: "full",
+              width: 'full',
+              paddingHorizontal: 0,
+            }}
+            inputContainerStyle={{
+              width: '100%',
               height: 60,
               backgroundColor: "white",
+              borderBottomColor: 'white',
               borderRadius: 20,
+              paddingHorizontal: 10
             }}
             placeholder="Mật khẩu"
             secureTextEntry={!isPasswordVisible}
             leftIcon={{ type: "feather", name: "key",  }}
             rightIcon={{ type: "feather", name: isPasswordVisible ? "eye-off" : "eye" , onPress: () => setPasswordVisible(!isPasswordVisible) }}
           />
-          <Text className="text-right text-base text-[#2B308B]">
+          <Text className="mb-5 text-right text-base text-[#2B308B]">
             Quên mật khẩu?
           </Text>
           <Button
@@ -48,6 +60,9 @@ const Login = () => {
               height: 50,
               backgroundColor: "#F490AF",
               borderRadius: 20,
+            }}
+            titleStyle={{
+             fontWeight: 'semibold'
             }}
           />
         </View>
@@ -59,7 +74,7 @@ const Login = () => {
         </View>
         <View className="flex flex-row gap-2 text-base">
           <Text>Chưa có tài khoản?</Text>
-          <Text className="text-[#2B308B] font-bold">Đăng ký</Text>
+          <Text className="text-[#2B308B] font-bold" onPress={() => navigation.navigate('Register')}>Đăng ký</Text>
         </View>
       </View>
     </SafeAreaView>
