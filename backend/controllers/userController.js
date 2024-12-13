4e51402e3291623cb7e4007ae1bfe8b69a843042
ryderthieu <ryderthieu@gmai.com>
2024-12-13 15:05:25 +0700

    update email confirm

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken')
-
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt')
 
 const createToken = (_id) => {
     return jwt.sign({_id}, process.env.SECRET, {expiresIn: '5d'})
@@ -35,12 +36,79 @@ const signup = async (req, res) => {
     }
 }
 
+const sendOtpEmail = (email, otp) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
 
-const getProfile = async (req, res) => {
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: 'Mã OTP Xác Nhận Đặt Lại Mật Khẩu',
+        text: `Mã OTP của bạn để đặt lại mật khẩu là: ${otp}`,
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.error('Error sending email:', err);
+        } else {
+            console.log('OTP sent:', info.response);
+        }
+    });
+};
+
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+
     try {
-        const user = await User.findById(req.userID)
-    } catch {
-        
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error('Email không tồn tại!');
+        }
+
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        user.otp = otp;
+        user.otpExpire = Date.now() + 3600000;
+        await user.save();
+
+        sendOtpEmail(email, otp);
+
+        res.status(200).json({ message: 'Mã OTP đã được gửi tới email của bạn!' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-}
-module.exports = {login, signup}
+};
+
+const resetPassword = async (req, res) => {
+    const { otp, newPassword } = req.body;
+
+    try {
+        const user = await User.findOne({ otp });
+
+        if (!user) {
+            throw new Error('OTP không hợp lệ!');
+        }
+
+        if (user.otpExpire < Date.now()) {
+            throw new Error('Mã OTP đã hết hạn!');
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+        user.password = hashedPassword;
+        user.otp = undefined; 
+        user.otpExpire = undefined; 
+        await user.save();
+
+        res.status(200).json({ message: 'Mật khẩu đã được cập nhật thành công!' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = {login, signup, forgotPassword, resetPassword}
