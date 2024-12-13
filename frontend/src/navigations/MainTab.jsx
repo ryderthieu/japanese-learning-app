94ea0d06ba87d530d118ee3228ee48f885985650
ryderthieu <ryderthieu@gmai.com>
2024-12-14 00:58:47 +0700

    update ?

--- frontend/src/navigations/MainTab.jsx
+++ frontend/src/navigations/MainTab.jsx
@@ -1,12 +1,13 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Home from "../screens/Home"
+import Courses from "../screens/Courses"
 
 const MainTab = () => {
   const Tab = createBottomTabNavigator()
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={Home}/>
-
+      <Tab.Screen name="Courses" component={Courses}/>
     </Tab.Navigator>
   )
 }
