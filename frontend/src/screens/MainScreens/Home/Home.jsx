6c4341d555335df429cd538256ef7d87c90d8e0e
ryderthieu <ryderthieu@gmai.com>
2024-12-14 11:18:40 +0700

    fix-home

--- frontend/src/screens/MainScreens/Home/Home.jsx
+++ frontend/src/screens/MainScreens/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { Dimensions, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import Carousel from 'react-native-reanimated-carousel';
 import MyCarousel from './MyCarousel';
 
 const Home = () => {
