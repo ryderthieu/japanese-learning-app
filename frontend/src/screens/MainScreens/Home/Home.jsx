3522c3bf458a4bdb907d309b9e873947e1635599
Tran Nhat Truong <22521584@gm.uit.edu.vn>
2024-12-14 11:06:55 +0700

    update home screnn part1

--- frontend/src/screens/MainScreens/Home/Home.jsx
+++ frontend/src/screens/MainScreens/Home/Home.jsx
@@ -1,15 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Carousel from 'react-native-reanimated-carousel';
+import MyCarousel from './MyCarousel';
 
 const Home = () => {
   return (
     <SafeAreaView>
       <Text>Home</Text>
+      <MyCarousel />
     </SafeAreaView>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
