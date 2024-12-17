0137c47c2fbfef209f43664297b339c16ea687df
Tran Nhat Truong <22521584@gm.uit.edu.vn>
2024-12-17 20:56:24 +0700

    update vocab & kanji

---
+++ frontend/src/components/VocabCard/VocabPagination.jsx
@@ -0,0 +1,55 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import Animated, { interpolate, useAnimatedStyle, Extrapolation } from "react-native-reanimated";
+import { Dimensions } from "react-native";
+
+const { width } = Dimensions.get("screen");
+
+const VocabPagination = ({ items, paginationIndex, scrollX }) => {
+  return (
+    <View style={styles.container}>
+      {items.map((_, index) => {
+        const pgAnimationStyle = useAnimatedStyle(() => {
+          const dotWidth = interpolate(
+            scrollX.value % (items.length * width),
+            [(index - 1) * width, index * width, (index + 1) * width],
+            [8, 20, 8],
+            Extrapolation.CLAMP
+          );
+          return {
+            width: dotWidth,
+          };
+        });
+
+        return (
+          <Animated.View
+            key={index}
+            style={[
+              styles.dot,
+              pgAnimationStyle,
+              { backgroundColor: paginationIndex === index ? "#222" : "#aaa" },
+            ]}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+export default VocabPagination;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dot: {
+    backgroundColor: "#aaa",
+    height: 8,
+    width: 8,
+    marginHorizontal: 2,
+    borderRadius: 8,
+  },
+});
